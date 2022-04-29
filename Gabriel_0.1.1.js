6f1e0a5858aeda708f521b9ed465caeb49505a80
Immortalxw <gabriel.menezesxw@gmail.com>
2022-04-29 20:46:09 -0300

    1 file deleted

--- Gabriel_0.1.1.js
+++
@@ -1,8 +0,0 @@
-function calcularBoleto(valorBoleto, diasAtraso) {
-  const taxa = 0.00033;
-  const valorTaxa = valorBoleto * taxa;
-  const valorMulta = valorTaxa * diasAtraso;
-  const valorAtualizado = valorBoleto + valorMulta;
-  return valorAtualizado;
-}
-console.log(calcularBoleto(1000, 1));
