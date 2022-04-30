1c61cc8cfb88c43302820a493616b3f9da0a9c4f
Immortalxw <gabriel.menezesxw@gmail.com>
2022-04-29 21:03:04 -0300

    1 file added

---
+++ gabriel_0.2.1.js
@@ -0,0 +1,43 @@
+const filmes = [
+  {
+    nome: 'O Senhor do Aneis: As duas torretas',
+    lancamento: 2002,
+    diretores: ['Ronald Mc Donald'],
+    generos: ['Fantasia', 'Aventura'],
+    boxOfice,
+  },
+  {
+    nome: 'Harry Potter e ordem da Lamburghini',
+    lancamento: 2003,
+    diretores: ['Luis inácio luigi da silva'],
+    generos: ['Fantasia', 'Magia'],
+    boxOfice:[
+      America = 1_263_788,
+      Oceania = 125_516,
+      Europa = 121_789,
+      Asia = 1_241_256,
+      Antártida = 3,
+    ]
+  },
+  {
+    nome: 'Matrix Recarregado',
+    lancamento: 2035,
+    diretores: ['Keanu Reaves', 'Keanu Reaves - 04'],
+    generos: ['Ação', 'Ficção Cientifica', 'Games '],
+    boxOfice:[
+      America = 21_002_310,
+      Oceania = 20_102,
+      Europa = 1_011_1345,
+      Asia = 1_203_781_023,
+      Antártida = 5,
+    ]
+  }
+]
+
+const boxOfice = [
+  {America : 21_002_310},
+  {Oceania : 20_102},
+  {Europa : 1_010_111_345},
+  {Asia : 12_031_023},
+  {Antártida : 4},
+]
